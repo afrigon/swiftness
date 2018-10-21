8aa7ce0c9e52571701f626261d344e786c9b7a0d
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2018-10-21 01:28:27 -0400

    Initial Commit

---
+++ swift-nes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  swift-nes
+//
+//  Created by Alexandre Frigon on 2018-10-21.
+//  Copyright © 2018 Frigstudio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
