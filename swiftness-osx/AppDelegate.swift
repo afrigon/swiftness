fce580434b52810536f757af105d88fa701e0c27
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-01-19 09:43:31 -0500

    removed pods

--- swiftness-osx/AppDelegate.swift
+++ swiftness-osx/AppDelegate.swift
@@ -38,8 +38,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.options.romURL = URL(string: "file:///Users/frigon/Downloads/nes/palette.nes")
         self.options.romURL = URL(string: "file:///Users/frigon/.nes/roms/donkey-kong.nes")
         
-        self.options.romURL = URL(string: "file:///Users/frigon/.nes/roms/zelda.nes")
         self.options.romURL = URL(string: "file:///Users/frigon/.nes/roms/lesbian-tennis.nes")
+        self.options.romURL = URL(string: "file:///Users/frigon/.nes/roms/zelda.nes")
     }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
